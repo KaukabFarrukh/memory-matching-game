15bd113fe4c61c6caec864717cfea0ed9b930c12
neslihannsrm <neslihannsrm@gmail.com>
2024-12-08 23:26:40 +0100

    modified instructions

--- src/components/InstructionsModal.js
+++ src/components/InstructionsModal.js
@@ -5,7 +5,7 @@ const InstructionsModal = ({ onClose }) => {
   return (
     <div className="modal-overlay">
       <div className="modal-content">
-        <h2>Instructions</h2>
+        <h1>Instructions</h1>
         <p>Find matching pairs and earn points!</p>
         <h3>How to Play:</h3>
         <ul>
@@ -22,9 +22,8 @@ const InstructionsModal = ({ onClose }) => {
           </li>
           <li>Time Limit: You have only 15 seconds to complete all the matches!</li>
         </ul>
-        <h3>Tips:</h3>
         <p>Use your memory to remember where you saw the cards. Be quick and careful!</p>
-        <p>Ready? After entering your details, click the Login button to start matching.</p>
+        <p>Ready? Close the instructions and start matching!</p>
         <button className="modal-close-button" onClick={onClose}>
           Close
         </button>
